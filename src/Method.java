000ac09ecfcf20523b4a945d14fd6aca97759e57
SIGMA1425 <yahasho1425@gmail.com>
2020-01-28 11:46:53 +0900

    Method.javaを作り，addメソッドを書いた

---
+++ src/Method.java
@@ -0,0 +1,12 @@
+public class Method
+{
+    public static void main(String[] args)
+    {
+        System.out.println("add(10, 5) -> " + add(10, 5));
+    }
+
+    public static int add(int x, int y)
+    {
+        return x+y;
+    }
+}
