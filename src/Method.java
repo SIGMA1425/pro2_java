7a7094d8129700620e6e54e839b985c8a15a9efd
SIGMA1425 <yahasho1425@gmail.com>
2020-01-28 11:55:15 +0900

    Method.javaにaryAveメソッドを追記

--- src/Method.java
+++ src/Method.java
@@ -4,6 +4,9 @@ public class Method
     {
         System.out.println("add(10, 5) -> " + add(10, 5));
         System.out.println("select_bigger(10.01, 5.94) -> " + select_bigger(10.01, 5.94));
+        double[] ary = {4, 6, 3, 7, 5};
+        System.out.println("aryAve(ary) -> " + aryAve(ary));
+        System.out.println("ary = {4, 6, 3, 7, 5}");
     }
 
     public static int add(int x, int y)
@@ -14,5 +17,16 @@ public class Method
     public static double select_bigger(double x, double y)
     {
         return x > y? x:y;
+    }
+
+    public static double aryAve(double[] dAry)
+    {
+        double sum=0;
+        for(int i=0; i<dAry.length; i++)
+        {
+            sum += dAry[i];
+        }
+
+        return sum/dAry.length;
     }
 }
