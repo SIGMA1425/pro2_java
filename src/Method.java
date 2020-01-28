45c29a67e1acaeaa822042ef6422818e6e745a3e
SIGMA1425 <yahasho1425@gmail.com>
2020-01-28 11:50:19 +0900

    Method.javaにselect_biggerメソッドを追記

--- src/Method.java
+++ src/Method.java
@@ -3,10 +3,16 @@ public class Method
     public static void main(String[] args)
     {
         System.out.println("add(10, 5) -> " + add(10, 5));
+        System.out.println("select_bigger(10.01, 5.94) -> " + select_bigger(10.01, 5.94));
     }
 
     public static int add(int x, int y)
     {
         return x+y;
+    }
+
+    public static double select_bigger(double x, double y)
+    {
+        return x > y? x:y;
     }
 }
